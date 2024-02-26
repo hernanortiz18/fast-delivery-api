aa3f01a3116973462d2a0adca97d20e920bff8d6
McHeph <juanarismendidiaz@gmail.com>
2024-02-26 13:26:20 -0300

    fix: change from try catch to .then

---
+++ src/routes/packages.routes.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { PackagesControllers } from '../controllers/packages.controllers'
+import { validateAuth } from '../middlewares/validateAuth.middlewares'
+const router = express.Router()
+
+router.get('/', validateAuth, PackagesControllers.getAllPackages)
+router.get('single/:id', validateAuth, PackagesControllers.getPackageById)
+router.get('status/:status', validateAuth, PackagesControllers.getPackagesByStatus)
--- src/routes/pakcages.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { PackagesControllers } from "../controllers/packages.controllers";
-const router = express.Router();
-import { validateAuth } from "../middlewares/validateAuth.middlewares";
-
-router.get("/", validateAuth, PackagesControllers.getAllPackages);
-router.get("single/:id", validateAuth, PackagesControllers.getPackageById)
-router.get("status/:status", validateAuth, PackagesControllers.getPackagesByStatus)
