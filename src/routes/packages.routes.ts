4f39514697b636797b329ce0a88ff9009697511e
fsosa44 <sosafrancisco2005@gmail.com>
2024-03-13 15:41:18 -0300

    swagged added

--- src/routes/packages.routes.ts
+++ src/routes/packages.routes.ts
@@ -4,6 +4,30 @@ import { validateAdmin, validateAuth } from '../middlewares/validateAuth.middlew
 const router = express.Router()
 
 router.get('/', validateAuth, PackagesControllers.getAllPackages)
+/**
+ * @swagger
+ * /api/packages/:
+ *   get:
+ *     summary: Obtener todos los paquetes
+ *     description: Obtiene una lista de todos los paquetes.
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: Lista de paquetes obtenida exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Package'
+ *       '401':
+ *         description: No autorizado, token inválido o ausente
+ *       '500':
+ *         description: Error al realizar la solicitud
+ */
+router.get('/', validateAuth, PackagesControllers.getAllPackages)
+
 router.get('/single/:id', validateAuth, PackagesControllers.getPackageById)
 router.get('/status/:status', validateAuth, PackagesControllers.getPackagesByStatus)
 router.get('/driver/:id', validateAuth, PackagesControllers.getPackagesByDriver)
